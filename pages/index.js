d29eaa831e9bbf83f1a104c13ac9d4599cce3c5e
Agarey <alexdrumm13@gmail.com>
2022-10-10 09:57:17 +0600

    application error fix

--- pages/index.js
+++ pages/index.js
@@ -17,33 +17,33 @@ import dynamic from 'next/dynamic'
 
 const Main = (props) => {
 
-  const ym = () => {
-    return (
-      "<!-- Yandex.Metrika counter -->\n" +
-      "<script type=\"text/javascript\" >\n" +
-      "   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};\n" +
-      "   m[i].l=1*new Date();\n" +
-      "   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}\n" +
-      "   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n" +
-      "   (window, document, \"script\", \"https://mc.yandex.ru/metrika/tag.js\", \"ym\");\n" +
-      "   ym(90703823, \"init\", {\n" +
-      "        clickmap:true,\n" +
-      "        trackLinks:true,\n" +
-      "        accurateTrackBounce:true,\n" +
-      "        webvisor:true\n" +
-      "   });\n" +
-      "</script>\n" +
-      "<noscript><div><img src=\"https://mc.yandex.ru/watch/90703823\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>\n" +
-      "<!-- /Yandex.Metrika counter -->"
-    );
-  };
+  // const ym = () => {
+  //   return (
+  //     "<!-- Yandex.Metrika counter -->\n" +
+  //     "<script type=\"text/javascript\" >\n" +
+  //     "   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};\n" +
+  //     "   m[i].l=1*new Date();\n" +
+  //     "   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}\n" +
+  //     "   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n" +
+  //     "   (window, document, \"script\", \"https://mc.yandex.ru/metrika/tag.js\", \"ym\");\n" +
+  //     "   ym(90703823, \"init\", {\n" +
+  //     "        clickmap:true,\n" +
+  //     "        trackLinks:true,\n" +
+  //     "        accurateTrackBounce:true,\n" +
+  //     "        webvisor:true\n" +
+  //     "   });\n" +
+  //     "</script>\n" +
+  //     "<noscript><div><img src=\"https://mc.yandex.ru/watch/90703823\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>\n" +
+  //     "<!-- /Yandex.Metrika counter -->"
+  //   );
+  // };
 
   return (
     <div className={styles.main}>
-      <Head>
+      {/*<Head>
         <title>OilanClassroom</title>
         <div dangerouslySetInnerHTML={{__html: ym()}}/>
-      </Head>
+      </Head>*/}
       <Header white={true}/>
       <AboutCourse/>
       <AboutTeacher/>

45c2ae09db269345f5cb9bcf4dd17f3b908e99b1
Agarey <alexdrumm13@gmail.com>
2022-10-10 11:43:10 +0600

    application error fix10

--- pages/index.js
+++ pages/index.js
@@ -19,22 +19,19 @@ const Main = (props) => {
 
   const ym = () => {
     return (
-      "<!-- Yandex.Metrika counter -->\n" +
-      "<script type=\"text/javascript\" >\n" +
-      "   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};\n" +
-      "   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n" +
-      "   (window, document, \"script\", \"https://mc.yandex.ru/metrika/tag.js\", \"ym\");\n" +
-      "\n" +
-      "   ym(90703823, \"init\", {\n" +
-      "        clickmap:true,\n" +
-      "        trackLinks:true,\n" +
-      "        accurateTrackBounce:true,\n" +
-      "        webvisor:true,\n" +
-      "        ecommerce:\"dataLayer\"\n" +
-      "   });\n" +
-      "</script>\n" +
-      "<noscript><div><img src=\"https://mc.yandex.ru/watch/90703823\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>\n" +
-      "<!-- /Yandex.Metrika counter -->"
+      "<script src='https://mc.yandex.ru/metrika/watch.js' type='text/javascript'></script>\
+      <script type='text/javascript'>\
+            try {\
+                  var yaCounter90703823 = new Ya.Metrika({\
+                  id:90703823,\
+                  clickmap:true,\
+                  trackLinks:true,\
+                  accurateTrackBounce:true,\
+                  webvisor:true,\
+                  trackHash:true\
+                  });\
+            } catch(e) { }\
+      </script>"
     );
   };
 

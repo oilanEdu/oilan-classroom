ae5ced707f2bd1ea51c34c709292267ffefba7c4
Agarey <alexdrumm13@gmail.com>
2022-10-10 10:57:37 +0600

    application error fix9

--- pages/index.js
+++ pages/index.js
@@ -22,15 +22,15 @@ const Main = (props) => {
       "<!-- Yandex.Metrika counter -->\n" +
       "<script type=\"text/javascript\" >\n" +
       "   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};\n" +
-      "   m[i].l=1*new Date();\n" +
-      "   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}\n" +
-      "   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n" +
+      "   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n" +
       "   (window, document, \"script\", \"https://mc.yandex.ru/metrika/tag.js\", \"ym\");\n" +
+      "\n" +
       "   ym(90703823, \"init\", {\n" +
       "        clickmap:true,\n" +
       "        trackLinks:true,\n" +
       "        accurateTrackBounce:true,\n" +
-      "        webvisor:true\n" +
+      "        webvisor:true,\n" +
+      "        ecommerce:\"dataLayer\"\n" +
       "   });\n" +
       "</script>\n" +
       "<noscript><div><img src=\"https://mc.yandex.ru/watch/90703823\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>\n" +

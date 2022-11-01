849c675e789971c20b4c1a32ec87f20f45a52346
Agarey <alexdrumm13@gmail.com>
2022-11-01 09:37:18 +0600

    metricaFix

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,35 @@
 import '../styles/globals.css'
-import React, {useEffect, useState} from "react";
+import {useEffect} from "react";
 import { CookiesProvider } from 'react-cookie'
 import TagManager from "react-gtm-module";
 import { YMInitializer } from 'react-yandex-metrika';
 
 const tagManagerArgs = {
-    gtmId: 'GTM-MFV3BJ3'
+  gtmId: 'GTM-MFV3BJ3'
 }
 
 function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        document.documentElement.style.backgroundColor = '#F1FAFF';
-        document.body.style.overflowX = "hidden";
-        TagManager.initialize(tagManagerArgs);
-    }, []);
+  useEffect(() => {
+    document.documentElement.style.backgroundColor = '#F1FAFF';
+    TagManager.initialize(tagManagerArgs);
+  }, []);
 
-    return (
-        <>
-            <YMInitializer accounts={[90703823]} options={{webvisor: true, defer: true}} version="2" />
-              <CookiesProvider>
-                <Component {...pageProps} />
-              </CookiesProvider>
-        </>
-    )
-}
+  return <>
+    <YMInitializer 
+      accounts={[90703823]} 
+      options={{
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        trackHash: true,
+      }} 
+      version="2" 
+    />
+    <CookiesProvider>
+      <Component {...pageProps} />
+    </CookiesProvider>
+  </>
+};
 
-export default MyApp
+export default MyApp;

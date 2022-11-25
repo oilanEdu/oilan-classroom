7c34d6d9cb9e9febe8ac77e1c2b83dc6ba18edfa
Agarey <alexdrumm13@gmail.com>
2022-11-25 17:24:48 +0600

    translationFix7

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
 	transports: ["websocket"]
 }
 
-const socket = io('https://localhost:3031', options);
+const socket = io('http://localhost:3031', options);
 
 export default socket;

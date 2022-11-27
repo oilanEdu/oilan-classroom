e42467460fa880fd06d60efd452670eabbf684cd
Agarey <alexdrumm13@gmail.com>
2022-11-27 18:26:43 +0600

    translationFix13

--- src/socket/index.js
+++ src/socket/index.js
@@ -8,6 +8,6 @@ const options = {
 	transports: ["websocket"]
 }
 
-const socket = io("ws://91.201.215.148:3031", options);
+const socket = io("wss://91.201.215.148:3031", options);
 
 export default socket; 

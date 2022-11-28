f92f7133f90064c432e92d506b7cf4b3d7feb38d
Agarey <alexdrumm13@gmail.com>
2022-11-28 16:42:15 +0600

    translationFix17

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,21 +1,21 @@
 import {io} from 'socket.io-client';
 import globals from "../globals";
 
-// const options = {
-// 	"force new connection": true,
-// 	reconnectionAttempts: "Infinity",
-// 	timeout: 10000,
-// 	transports: ["websocket"]
-// }
+const options = {
+	"force new connection": true,
+	reconnectionAttempts: "Infinity",
+	timeout: 10000,
+	transports: ["websocket"]
+}
 
-// const socket = io("wss://realibi.kz:3031", options);
-const socket = io(ENDPOINT, {  
-        cors: {
-        origin: "wss://realibi.kz:3031",
-        credentials: true
-      },transports : ['websocket'],
-      	"force new connection": true,
-		reconnectionAttempts: "Infinity",
-		timeout: 10000 });
+const socket = io("wss://realibi.kz:3031", options);
+// const socket = io(ENDPOINT, {  
+//         cors: {
+//         origin: "wss://realibi.kz:3031",
+//         credentials: true
+//       },transports : ['websocket'],
+//       	"force new connection": true,
+// 		reconnectionAttempts: "Infinity",
+// 		timeout: 10000 });
 
 export default socket; 

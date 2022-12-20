72d73bba8872ae1e128a45036a6b0831d4cea15c
Agarey <alexdrumm13@gmail.com>
2022-12-20 10:11:56 +0300

    metaFix

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import TagManager from "react-gtm-module";
 import { YMInitializer } from 'react-yandex-metrika';
 import Head from 'next/head';
 import { HMSRoomProvider } from '@100mslive/react-sdk';
+import { Helmet } from 'react-helmet';
 
 const tagManagerArgs = {
   gtmId: 'GTM-MFV3BJ3'
@@ -17,10 +18,26 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return <>
-    <Head>
+    <Helmet>
       <title>Oilan-classroom</title>
       <link rel="icon" href="https://realibi.kz/file/963267.png" />
-    </Head>
+      <meta name="description" content="знакомство с английским языком; лучшие преподаватели; от уровня A1 до уровня C1; подготовка к сдаче IELTS и TOEFL" />
+      <meta property="og:title" content="курсы английского онлайн" />
+      <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "name": "Oilan.io Ltd",
+              "url": "https://www.oilan-classroom.com/",
+              "sameAs": [
+                "https://www.oilan.io/",
+                "https://www.instagram.com/oilan.io/"]
+            }`
+          }}
+      />
+    </Helmet>
     <YMInitializer 
       accounts={[90703823]} 
       options={{

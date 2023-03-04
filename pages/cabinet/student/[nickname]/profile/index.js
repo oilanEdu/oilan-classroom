8458a16244a1fd513dff62d6360460d7370b0668
SoTasty <turboslfi@gmail.com>
2023-03-04 23:26:17 +0600

    new files

---
+++ pages/cabinet/student/[nickname]/profile/index.js
@@ -0,0 +1,274 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, {useEffect, useState, useRef} from "react";
+import Footer from "../../../../../src/components/Footer/Footer";
+import HeaderStudent from "../../../../../src/components/HeaderStudent/HeaderStudent";
+import HeaderTeacher from "../../../../../src/components/HeaderTeacher/HeaderTeacher";
+import globals from "../../../../../src/globals";
+import styles from './styles.module.css'
+///getTeacherByUrl/:url
+// фамилия имя отчество, почта, телефон, пароль, о себе, навыки, опыт работы, никнеймн-логин лучше логин
+function StudentProfile(props) {
+    const [files, setFiles] = useState([]);
+    const [fileName, setFileName] = useState()
+    const [link, setLink] = useState()
+    const [student, setStudent] = useState()
+    const [studentFIO, setStudentFIO] = useState()
+    const [studentEmail, setStudentEmail] = useState()
+    const [studentPhone, setStudentPhone] = useState()
+    const [studentPassword, setStudentPassword] = useState()
+    const [studentAboutSelf, setStudentAboutSelf] = useState()
+    const [studentSkills, setStudentSkills] = useState()
+    const [studentExp, setStudentExp] = useState()
+    const [studentLogin, setStudentLogin] = useState()
+    const [courseUrl, setCourseUrl] = useState()
+    const [passwordIsChanging, setPasswordIsChanging] = useState(false)
+    const [passwordIsChanged, setPasswordIsChanged] = useState()
+    const [studentLoginIsChanged, setStudentLoginIsChanged] = useState(false)
+    useEffect(() => {
+        console.log(studentLoginIsChanged, "studentLoginIsChanged");
+    }, [studentLoginIsChanged])
+    const router = useRouter();
+
+    const handleSubmit = (event) => {
+        files
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', files[0]);
+        axios.post(`${globals.productionServerDomain}/file/upload`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+          }).then((response) => {
+            console.log(response)
+              const uploadedFileName = response.data;
+              console.log('uploadedFileName', uploadedFileName)
+              setFileName(uploadedFileName)
+              const filePath = `${globals.ftpDomain}${uploadedFileName}`;
+              console.log('filePath', filePath)
+              setLink(filePath)
+          }).catch((error) => {
+              console.log(error);
+          });
+    }
+
+    // updateTeacherData
+    // const handleSubmitNewTeacherData = async() => { 
+    //     let studentFIOsplitter = studentFIO.split(' ')
+    //     const data = {
+    //       surname: studentFIOsplitter[0],
+    //       name: studentFIOsplitter[1],
+    //       patronymic: studentFIOsplitter[2],
+    //       email: studentEmail,
+    //       phone: studentPhone,
+    //       skills: studentSkills,
+    //       experience: studentExp, 
+    //       avatar: fileName,
+    //       url: studentLogin,
+    //       studentDescription: studentAboutSelf 
+    //     }; 
+    
+    //     console.log(data);
+    //     await axios({
+    //       method: "put",
+    //       url: `${globals.productionServerDomain}/updateTeacherData/` + 12, 
+    //       data: data,
+    //     })
+        
+    //     // if (studentLoginIsChanged === true) {
+    //     //     console.log("IMHERE");
+    //     //     router.push('/auth')
+    //     // }
+    //   };
+
+    const handleSubmitNewStudentData = async() => { 
+        let studentFIOsplitter = studentFIO.split(' ')
+        const data = {
+          surname: studentFIOsplitter[0],
+          name: studentFIOsplitter[1],
+          patronymic: studentFIOsplitter[2],
+          email: studentEmail,
+          phone: studentPhone,
+          img: fileName,
+          id: student.id,
+          nickname: studentLogin
+        }; 
+      
+        console.log(data);
+      
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateStudentDataFromProfile`,
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+
+        if (studentLoginIsChanged) {
+            updateStudentLogin()   
+        }
+        if (passwordIsChanged) {
+            updateStudentPassword()
+        }
+      };
+
+    const getStudentCoursesAndPrograms = async () => {
+        const response = await axios.get(`${globals.productionServerDomain}/getAllCoursesAndProgramsOfStudent?studentId=${student.id}`)
+        const data = {
+            courseId: response.data[0].course_id
+          }
+        let result = await axios.post(`${globals.productionServerDomain}/getCourseById`, data)
+        setCourseUrl(result.data[0].url)
+    }
+    useEffect(() => {
+        if (student != undefined) {
+            getStudentCoursesAndPrograms()
+        }
+    }, [student])
+    const loadStudentProfileInfo = async () => {
+        const studentUrl = props.nickname
+        const data = { 
+            studentUrl
+          }
+        // const getStudentInfo = await axios.post(`${globals.productionServerDomain}/getStudentByUrl` + data)
+        let getStudent = await axios({
+            method: "post",
+            url: `${globals.productionServerDomain}/getStudentByUrl`,
+            data: data,
+          })
+            .then(function (res) {
+              if (res.data[0]){
+                const studentInfo = res.data[0]
+                console.log(studentInfo, "studentInfo");
+                setStudent(studentInfo)
+                setStudentFIO(studentInfo.surname + ' ' + studentInfo.name + ' ' + studentInfo.patronymic)
+                setStudentEmail(studentInfo.email)
+                setStudentPhone(studentInfo.phone)
+                setStudentPassword()
+                setStudentLogin(studentInfo.nickname)
+              }else{
+
+              }
+            })
+            .catch((err) => {
+              alert("Произошла ошибка");   
+            });
+    }
+    useEffect(() => {
+        loadStudentProfileInfo()
+    }, [])
+
+    const updateStudentLogin = async () => {
+        const data = {
+            nick: studentLogin,
+            id: student.id,
+            roleId: 3
+        }; 
+        console.log(data);
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateUserLogin`,
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    const updateStudentPassword = async () => {
+        const data = {
+            password: studentPassword,
+            id: student.id,
+            roleId: 3
+        }; 
+        console.log(data);
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateUserPassword`,
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+	return ( 
+        <>
+            <HeaderStudent white={true} name={studentFIO?.split(' ')[1]} surname={studentFIO?.split(' ')[0]} courseUrl={courseUrl} nickname={studentLogin} />
+            <div className={styles.mainWrapperOfEditProgram} 
+            >
+                {/* <HeaderTeacher white={true} student={student} /> */}
+
+                <div className={styles.cantainer}>
+                    <div className={styles.mainTitle}>
+                        <span>Профиль</span>
+                    </div>
+                    <div className={styles.programBlock}>
+                        <h2>Фото профиля</h2> 
+                        <form onSubmit={handleSubmit}>
+                            <input type="file" name="file" onChange={(event) => setFiles(event.target.files)} />
+                            <input type="submit" value="Upload" />
+                        </form>
+                        <h2>Ф.И.О</h2> 
+                        <input type="text" 
+                            onChange={(event) => setStudentFIO(event.target.value)}
+                            value={studentFIO}/>
+                        <h2>Почта</h2>
+                        <input type="text" 
+                            onChange={(event) => setStudentEmail(event.target.value)}
+                            value={studentEmail}/>
+                        <h2>Телефон</h2>
+                        <input type="text" 
+                            onChange={(event) => setStudentPhone(event.target.value)}
+                            value={studentPhone}/>
+                        <h2>Пароль</h2>
+                        <button style={{display: passwordIsChanging ? 'none' : 'block'}} onClick={() => setPasswordIsChanging(true)}>Изменить пароль</button>
+                        <input type="password" 
+                            style={{display: passwordIsChanging ? 'block' : 'none'}}
+                            onChange={(event) => setStudentPassword(event.target.value)}
+                            value={studentPassword}/>
+                        <button style={{display: passwordIsChanging ? 'block' : 'none'}} 
+                            onClick={() => {setPasswordIsChanging(false)
+                                setPasswordIsChanged(true)
+                            }}>OK
+                        </button>
+                        <h2>Логин</h2>
+                        <input type="text" 
+                        // disabled
+                            onChange={(event) => {setStudentLogin(event.target.value)
+                                                setStudentLoginIsChanged(true)}}
+                            value={studentLogin}/>
+                        <h2>Save</h2>
+                        <button onClick={() => {
+                            handleSubmitNewStudentData()
+                            // debugger
+                           if (studentLoginIsChanged === true) {
+                            router.push(`/cabinet/student/${studentLogin}/profile`)
+                            } 
+                        }}>
+                            Save
+                        </button>
+                    </div>
+                </div>
+                
+                <Footer />
+            </div>
+        </>
+       )
+}
+
+StudentProfile.getInitialProps = async (ctx) => {
+	console.log('lol',ctx)
+    if(ctx.query.nickname !== undefined) {
+        return {
+            nickname: ctx.query.nickname
+        }
+    }else{
+        return {};
+    }
+}
+
+export default StudentProfile
---
+++ pages/cabinet/teacher/[url]/profile/index.js
@@ -0,0 +1,260 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, {useEffect, useState, useRef} from "react";
+import Footer from "../../../../../src/components/Footer/Footer";
+import HeaderTeacher from "../../../../../src/components/HeaderTeacher/HeaderTeacher";
+import globals from "../../../../../src/globals";
+import styles from './styles.module.css'
+///getTeacherByUrl/:url
+// фамилия имя отчество, почта, телефон, пароль, о себе, навыки, опыт работы, никнеймн-логин лучше логин
+function TeacherProfile(props) {
+    const [files, setFiles] = useState([]);
+    const [fileName, setFileName] = useState()
+    const [link, setLink] = useState()
+    const [teacher, setTeacher] = useState()
+    const [teacherFIO, setTeacherFIO] = useState()
+    const [teacherEmail, setTeacherEmail] = useState()
+    const [teacherPhone, setTeacherPhone] = useState()
+    const [teacherPassword, setTeacherPassword] = useState()
+    const [teacherAboutSelf, setTeacherAboutSelf] = useState()
+    const [teacherSkills, setTeacherSkills] = useState()
+    const [teacherExp, setTeacherExp] = useState()
+    const [teacherLogin, setTeacherLogin] = useState()
+    const [passwordIsChanging, setPasswordIsChanging] = useState(false)
+    const [passwordIsChanged, setPasswordIsChanged] = useState()
+    const [teacherLoginIsChanged, setTeacherLoginIsChanged] = useState(false)
+    useEffect(() => {
+        console.log(teacherLoginIsChanged, "teacherLoginIsChanged");
+    }, [teacherLoginIsChanged])
+    const router = useRouter();
+
+    const handleSubmit = (event) => {
+        files
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', files[0]);
+        axios.post(`${globals.productionServerDomain}/file/upload`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+          }).then((response) => {
+            console.log(response)
+              const uploadedFileName = response.data;
+              console.log('uploadedFileName', uploadedFileName)
+              setFileName(uploadedFileName)
+              const filePath = `${globals.ftpDomain}${uploadedFileName}`;
+              console.log('filePath', filePath)
+              setLink(filePath)
+          }).catch((error) => {
+              console.log(error);
+          });
+    }
+
+    const updateTeacherLogin = async () => {
+        const data = {
+            nick: teacherLogin,
+            id: teacher.id,
+            roleId: 2
+        }; 
+        debugger
+        console.log(data);
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateUserLogin`,
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    const updateTeacherPassword = async () => {
+        const data = {
+            password: teacherPassword,
+            id: teacher.id,
+            roleId: 2
+        }; 
+        console.log(data);
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateUserPassword`,
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    // updateTeacherData
+    // const handleSubmitNewTeacherData = async() => { 
+    //     let teacherFIOsplitter = teacherFIO.split(' ')
+    //     const data = {
+    //       surname: teacherFIOsplitter[0],
+    //       name: teacherFIOsplitter[1],
+    //       patronymic: teacherFIOsplitter[2],
+    //       email: teacherEmail,
+    //       phone: teacherPhone,
+    //       skills: teacherSkills,
+    //       experience: teacherExp, 
+    //       avatar: fileName,
+    //       url: teacherLogin,
+    //       teacherDescription: teacherAboutSelf 
+    //     }; 
+    
+    //     console.log(data);
+    //     await axios({
+    //       method: "put",
+    //       url: `${globals.productionServerDomain}/updateTeacherData/` + 12, 
+    //       data: data,
+    //     })
+        
+    //     // if (teacherLoginIsChanged === true) {
+    //     //     console.log("IMHERE");
+    //     //     router.push('/auth')
+    //     // }
+    //   };
+
+    const handleSubmitNewTeacherData = async() => { 
+        let teacherFIOsplitter = teacherFIO.split(' ')
+        const data = {
+          surname: teacherFIOsplitter[0],
+          name: teacherFIOsplitter[1],
+          patronymic: teacherFIOsplitter[2],
+          email: teacherEmail,
+          phone: teacherPhone,
+          skills: teacherSkills,
+          experience: teacherExp, 
+          avatar: fileName,
+          url: teacherLogin,
+          teacherDescription: teacherAboutSelf 
+        }; 
+      
+        console.log(data);
+      
+        try {
+          await axios({
+            method: "put",
+            url: `${globals.productionServerDomain}/updateTeacherData/` + teacher.id, 
+            data: data,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    const loadTeacherProfileInfo = async () => {
+        const getTeacherInfo = await axios.post(`${globals.productionServerDomain}/getTeacherByUrl/` + props.url)
+        const teacherInfo = getTeacherInfo.data[0]
+        console.log(getTeacherInfo, "getTeacherInfo");
+        setTeacher(teacherInfo)
+        setTeacherFIO(teacherInfo.surname + ' ' + teacherInfo.name + ' ' + teacherInfo.patronymic)
+        setTeacherEmail(teacherInfo.email)
+        setTeacherPhone(teacherInfo.phone)
+        setTeacherPassword()
+        setTeacherAboutSelf(teacherInfo.description)
+        setTeacherSkills(teacherInfo.skills)
+        setTeacherExp(teacherInfo.experience)
+        setTeacherLogin(teacherInfo.url)
+    }
+    useEffect(() => {
+        loadTeacherProfileInfo()
+    }, [])
+
+	return ( 
+        <>
+        <HeaderTeacher white={true} teacher={teacher} />
+            <div className={styles.mainWrapperOfEditProgram} 
+            >
+                {/* <HeaderTeacher white={true} teacher={teacher} /> */}
+
+                <div className={styles.cantainer}>
+                    <div className={styles.mainTitle}>
+                        <span>Профиль</span>
+                    </div>
+                    <div className={styles.programBlock}>
+                        <h2>Фото профиля</h2> 
+                        <form onSubmit={handleSubmit}>
+                            <input type="file" name="file" onChange={(event) => setFiles(event.target.files)} />
+                            <input type="submit" value="Upload" />
+                        </form>
+                        <h2>Ф.И.О</h2> 
+                        <input type="text" 
+                            onChange={(event) => setTeacherFIO(event.target.value)}
+                            value={teacherFIO}/>
+                        <h2>Почта</h2>
+                        <input type="text" 
+                            onChange={(event) => setTeacherEmail(event.target.value)}
+                            value={teacherEmail}/>
+                        <h2>Телефон</h2>
+                        <input type="text" 
+                            onChange={(event) => setTeacherPhone(event.target.value)}
+                            value={teacherPhone}/>
+                        <h2>Пароль</h2>
+                        <button style={{display: passwordIsChanging ? 'none' : 'block'}} onClick={() => setPasswordIsChanging(true)}>Изменить пароль</button>
+                        <input type="password" 
+                            style={{display: passwordIsChanging ? 'block' : 'none'}}
+                            onChange={(event) => setTeacherPassword(event.target.value)}
+                            value={teacherPassword}/>
+                        <button style={{display: passwordIsChanging ? 'block' : 'none'}} onClick={() => {setPasswordIsChanging(false)
+                                                                                                        setPasswordIsChanged(true)}}>OK</button>
+                        <h2>О себе</h2>
+                        <input type="text" 
+                            onChange={(event) => setTeacherAboutSelf(event.target.value)}
+                            value={teacherAboutSelf}/>
+                        <h2>Навыки</h2>
+                        <input type="text" 
+                            onChange={(event) => setTeacherSkills(event.target.value)}
+                            value={teacherSkills}/>
+                        <h2>Опыт работы</h2>
+                        <input type="text" 
+                            onChange={(event) => setTeacherExp(event.target.value)}
+                            value={teacherExp}/>
+                        <h2>Логин</h2>
+                        <input type="text" 
+                        // disabled
+                            onChange={(event) => {setTeacherLogin(event.target.value)
+                                                setTeacherLoginIsChanged(true)}}
+                            value={teacherLogin}/>
+                        <h2>Save</h2>
+                        <button onClick={() => {
+                            handleSubmitNewTeacherData()
+                            // debugger
+                            if (teacherLoginIsChanged) {
+                                updateTeacherLogin()
+                            }
+                            if (passwordIsChanged) {
+                                updateTeacherPassword()
+                            }
+                            if (teacherLoginIsChanged === true) {
+                                async function test () {
+                                    await localStorage.setItem('login', teacherLogin);
+                                    await router.push(`/cabinet/teacher/${teacherLogin}/profile`)
+                                    await window.location.reload()
+                                }
+                                test()
+                            } 
+                        }}>
+                            Save
+                        </button>
+                    </div>
+                </div>
+                
+                <Footer />
+            </div>
+        </>
+       )
+}
+
+TeacherProfile.getInitialProps = async (ctx) => {
+	console.log('lol',ctx)
+    if(ctx.query.url !== undefined) {
+        return {
+            url: ctx.query.url,
+        }
+    }else{
+        return {};
+    }
+}
+
+export default TeacherProfile

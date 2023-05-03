f634cec60e86cfe1c7e75c330b0a0d6eff293b35
Agarey <alexdrumm13@gmail.com>
2023-05-03 18:18:29 +0600

    new preview page

---
+++ pages/preview/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useMemo, useEffect } from 'react';
+import styles from './index.module.css'
+import logo from '../../src/resources/logo.png';
+
+const Index = () => {
+
+return <>
+    <div>LetsGo</div>
+  </>
+
+}
+
+export default Index;

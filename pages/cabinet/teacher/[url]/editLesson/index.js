aca39fcfb77ce53197a63b7e70cdf49c51e15017
Aldiyar Balkibekov <kofeeworld@mail.ru>
2023-10-13 20:41:32 +0600

    classroom update: material and attendance

--- pages/cabinet/teacher/[url]/editLesson/index.js
+++ pages/cabinet/teacher/[url]/editLesson/index.js
@@ -15,6 +15,7 @@ const createCourse = () => {
   const lessonId = router.query.lesson
   const [teacher, setTeacher] = useState([])
   const [baseDataLoaded, setBaseDataLoaded] = useState(false)
+  const [students, setStudents] = useState([]);
 
   const [lesson, setLesson] = useState()
   const [lessonTitle, setLessonTitle] = useState(lesson?.title)
@@ -26,6 +27,24 @@ const createCourse = () => {
   const [exerciseAnswer, setExerciseAnswer] = useState('')
 
   const [firstExerPress, setFirstExerPress] = useState(false)
+
+  const [localAttendance, setLocalAttendance] = useState([])
+
+  const [materialTitle, setMaterialTitle] = useState()
+  const [materialDescription, setMaterialDescription] = useState()
+  const [materialLink, setMaterialLink] = useState()
+  const [showMaterial, setShowMaterial] = useState(false)
+  const [dateAndTimeMerger, setDateAndTimeMerger] = useState()
+  const [dateState, setDateState] = useState();
+  const [timeState, setTimeState] = useState()
+  const [selectedOption, setSelectedOption] = useState('linkRadio');
+  const [links, setLinks] = useState(undefined)
+  const [materials, setMaterials] = useState()
+  const handleOptionChange = (event) => {
+      setSelectedOption(event.target.value);
+  };
+  const [files, setFiles] = useState([])
+
 
   useEffect(() => {
     if (lesson?.title) {
@@ -73,6 +92,44 @@ const createCourse = () => {
       // debugger
     })
 
+
+    const dataStudents = {
+      id: teacherIdLocal,
+      sort: 'sort'
+    }
+    let teacherStudents = await axios.post(`${globals.productionServerDomain}/getStudentsByTeacherId/`, dataStudents)
+
+    function filterByRepetitiveId(objects) {
+      // Group objects by ID
+      const groupedById = {};
+      objects.forEach((obj) => {
+        if (groupedById[obj.student_id]) {
+          groupedById[obj.student_id].push(obj);
+        } else {
+          groupedById[obj.student_id] = [obj];
+        }
+      });
+
+      // Filter objects by closest date
+      const result = [];
+      Object.values(groupedById).forEach((group) => {
+        if (group.length > 1) {
+          result.push(group[0]);
+        } else {
+          result.push(group[0]);
+        }
+      });
+
+      return result;
+    }
+    setStudents(filterByRepetitiveId(teacherStudents['data']))
+
+    const dataLesson = {
+      lesson_id: lessonId
+    }
+    let getLessonAttendance = await axios.post(`${globals.productionServerDomain}/getLessonAttendance/`, dataLesson)
+    setLocalAttendance(getLessonAttendance['data'])
+    debugger
   }
 
   const loadBaseDataSecond = async () => {
@@ -240,14 +297,150 @@ const createCourse = () => {
       addressUndefinedFixer()
     }
   }, [router])
-  
+
+  const attendanceAll = async () => {
+    let newArray = [...localAttendance]
+    for (let index = 0; index < students.length; index++) {
+      const element = students[index];
+      const data = {
+        person_id: element.student_id,
+        lesson_id: lessonId
+      }
+      const create = await axios.post(`${globals.productionServerDomain}/createAttendanceControlTeacher/`, data)
+      const newData = {
+        ...data,
+        id: create['data']['id']
+      }
+      newArray.push(newData)
+
+    }
+    setLocalAttendance(newArray)
+  }
+
+  const handleAttendanceSumbit = async (person_id, lesson_id) => {
+    const data = {
+      person_id,
+      lesson_id
+    }
+    const create = await axios.post(`${globals.productionServerDomain}/createAttendanceControlTeacher/`, data)
+
+    const newData = {
+      ...data,
+      id: create['data']['id']
+    }
+    let localAttendance2 = [...localAttendance]
+    localAttendance2.push(newData)
+    setLocalAttendance(localAttendance2)
+  }
+
+
+  const fetch = async () => {
+    const response = await axios.post(`${globals.productionServerDomain}/getLessonMaterialsOC`, { lessonId: router.query.lesson });
+    const links = response['data']['data'].filter(obj => obj.is_lesson_link === true);
+    const materials = response['data']['data'].filter(obj => obj.is_lesson_link === false);
+    setLinks(links)
+    setMaterials(materials)
+    console.log(materials, "materials")
+    console.log(links, "links")
+  }
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  const handleSubmit2 = async () => {
+    if (selectedOption === 'fileRadio') {
+      files
+      // event.preventDefault();
+      const formData = new FormData();
+      formData.append('file', files[0]);
+      axios.post(`${globals.productionServerDomain}/file/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(async (response) => {
+        console.log(response)
+        const uploadedFileName = response.data;
+        console.log('uploadedFileName', uploadedFileName)
+
+        let format = uploadedFileName.split('.').pop()
+        const data = {
+          title: materialTitle,
+          description: materialDescription,
+          file_type: format,
+          // link: materialLink,
+          link: selectedOption === 'fileRadio' ? uploadedFileName : materialLink,
+          is_lesson_link: false,
+          lesson_id: lessonId
+        }
+        try {
+          const create = await axios.post(`${globals.productionServerDomain}/addLessonMaterialOC/`, data)
+          if (create.status === 200) {
+            // addAlert("Материал успешно загружен", "accepted", alerts, setAlerts);
+
+            // setMaterialTitle('')
+            // setMaterialDescription('')
+            // setMaterialLink('')
+            // setShowMaterial(false)
+            // setSelectedOption('linkRadio')
+            fetch()
+          } else {
+            // addAlert("Ошибка при загрузке материала", "error", alerts, setAlerts);
+            // return { success: false, message: 'Error in creating program' };
+          }
+        } catch (error) {
+          // addAlert("Ошибка сети", "error", alerts, setAlerts);
+          // console.error('Error:', error);
+          // return { success: false, message: 'Network error or unexpected issue occurred' };
+        }
+
+
+        const filePath = `${globals.ftpDomain}${uploadedFileName}`;
+        console.log('filePath', filePath)
+        filePath
+      }).catch((error) => {
+        console.log(error);
+      });
+    } else {
+      const data = {
+        title: materialTitle,
+        description: materialDescription,
+        file_type: 'link',
+        // link: materialLink,
+        link: selectedOption === 'fileRadio' ? uploadedFileName : materialLink,
+        is_lesson_link: false,
+        lesson_id: lessonId
+      }
+      try {
+        const create = await axios.post(`${globals.productionServerDomain}/addLessonMaterialOC/`, data)
+        if (create.status === 200) {
+          // addAlert("Материал успешно загружен", "accepted", alerts, setAlerts);
+
+          // setMaterialTitle('')
+          // setMaterialDescription('')
+          // setMaterialLink('')
+          // setShowMaterial(false)
+          // setSelectedOption('linkRadio')
+          fetch()
+        } else {
+          // addAlert("Ошибка при загрузке материала", "error", alerts, setAlerts);
+          // return { success: false, message: 'Error in creating program' };
+        }
+      } catch (error) {
+        // addAlert("Ошибка сети", "error", alerts, setAlerts);
+        console.error('Error:', error);
+        // return { success: false, message: 'Network error or unexpected issue occurred' };
+      }
+
+    }
+  }
+
   return <>
-        <HeaderTeacher
-        white={true}
-        url={teacherUrl}
-        teacher={teacher}
-        isInMainPage={isInMainPage}
-      />
+    <HeaderTeacher
+      white={true}
+      url={teacherUrl}
+      teacher={teacher}
+      isInMainPage={isInMainPage}
+    />
     <div className={styles.container}>
 
       <GoToLessonWithTimerComponent isTeacher={true} url={router.query.url} />
@@ -259,23 +452,121 @@ const createCourse = () => {
             deleteLesson(lessonId)
           }}>Удалить урок</span>
         </div>
-        <div className={styles.input_container}>
-          <span>Название урока</span>
-          <input
-            className={styles.lessonTitle}
-            type="text"
-            value={lessonTitle}
-            placeholder="Текст"
-            onChange={(e) => setLessonTitle(e.target.value)}
-          />
-          <span>Описание урока</span>
-          <textarea
-            className={styles.lessonContent}
-            type="text"
-            value={lessonDesc}
-            placeholder="Текст"
-            onChange={(e) => setLessonDesc(e.target.value)}>
-          </textarea>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: 'space-between', marginBottom: '40px' }}>
+          <div style={{ width: '40%', marginRight: '40px' }} className={styles.input_container}>
+            <span>Название урока</span>
+            <input
+              className={styles.lessonTitle}
+              type="text"
+              value={lessonTitle}
+              placeholder="Текст"
+              onChange={(e) => setLessonTitle(e.target.value)}
+            />
+            <span>Описание урока</span>
+            <textarea
+              className={styles.lessonContent}
+              type="text"
+              value={lessonDesc}
+              placeholder="Текст"
+              onChange={(e) => setLessonDesc(e.target.value)}>
+            </textarea>
+
+            {/* ////////// */}
+            {/* {materials?.length > 0 ?
+              <div className={styles.lessonInfoBlock}>
+                <p style={{ marginBottom: '20px' }} className={styles.answer_text}>
+                  Материалы:
+                </p>
+                <div className={styles.materials}>
+                  {materials.map(el => (
+                    <div className={styles.material}
+                    // onClick={() => deleteMaterial(el.link)}
+                    >
+                      <img src="/icons/defaulFile.svg"></img>
+                      <p>{el.title}.{el.file_type}</p>
+                    </div>))}
+                </div>
+              </div>
+              : ''} */}
+            <div style={{marginTop: '60px'}} className={styles.lessonInfoBlock}>
+              <span className={styles.label}>Материал:</span>
+              <input className={styles.input} onChange={(e) => setMaterialTitle(e.target.value)} value={materialTitle} placeholder="Название"></input>
+              <input className={styles.input} onChange={(e) => setMaterialDescription(e.target.value)} value={materialDescription} placeholder="Описание"></input>
+            </div>
+            <div className={styles.lessonInfoBlock}>
+              <span className={styles.label}>Тип материала:</span>
+              <span 
+              style={{display: 'flex', flexDirection: 'row-reverse', alignItems: 'center'}}>
+                <input
+                  type="radio"
+                  id="radio1"
+                  name="radioGroup"
+                  value="linkRadio"
+                  checked={selectedOption === 'linkRadio'}
+                  onChange={handleOptionChange}
+                />
+                <label htmlFor="radio1">Ссылка на материал</label>
+              </span>
+              <span
+              style={{display: 'flex', flexDirection: 'row-reverse', alignItems: 'center'}}>
+                <input
+                  type="radio"
+                  id="radio2"
+                  name="radioGroup"
+                  value="fileRadio"
+                  checked={selectedOption === 'fileRadio'}
+                  onChange={handleOptionChange}
+                />
+                <label htmlFor="radio2">Приложить материал как файл</label>
+              </span>
+            </div>
+            <div className={styles.lessonInfoBlock}>
+              {selectedOption === 'fileRadio' ?
+                <input type="file" name="file" onChange={(event) => {
+                  setFiles(event.target.files)
+                }} />
+                :
+                <input className={styles.input} value={materialLink} onChange={(e) => setMaterialLink(e.target.value)} placeholder="Ссылка на материал"></input>
+              }
+            </div>
+            <div className={styles.lessonInfoBlock}>
+              <button className={styles.saveButton} onClick={() => handleSubmit2()}>Отправить материал</button>
+            </div>
+            {/* ////////// */}
+          </div>
+          <div className={styles.attendanceWrapper}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <span style={{ marginBottom: '40px' }}>Посещаемость урока</span>
+              <div style={{ display: 'flex', gap: '10px', cursor: 'pointer' }}
+                onClick={() => {
+                  attendanceAll()
+                }}>
+                <p>Отметить всех</p>
+                <img src="https://realibi.kz/file/638719.png"
+                  style={{ cursor: 'pointer', width: '24px', height: '24px' }}
+                />
+              </div>
+            </div>
+            <div style={{ display: 'flex', gap: '15px', flexDirection: 'column' }}>
+              {students.map(el => (
+                <div className={styles.attendanceItem} style={{ display: "flex" }}>
+                  <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+                    <img src="https://realibi.kz/file/100841.png" />
+                    <p>{el.name} {el.surname}</p>
+                  </div>
+                  {/* <input
+                                    checked={localAttendance.find(el2 => el2.lesson_id === lessonId && el2.person_id === el.id) === undefined ? false : true}
+                                    disabled={localAttendance.find(el2 => el2.lesson_id === lessonId && el2.person_id === el.id) === undefined ? false : true}
+                                    onChange={() => { handleAttendanceSumbit(el.id, lessonId) }}
+                                    type="checkbox"></input> */}
+                  <img src="https://realibi.kz/file/638719.png"
+                    style={{ cursor: 'pointer', opacity: (localAttendance.find(el2 => +el2.lesson_id === +lessonId && +el2.person_id === +el.student_id) === undefined ? false : true) ? '0.4' : '1' }}
+                    onClick={() => { handleAttendanceSumbit(el.student_id, lessonId) }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <div className={styles.exercisesData}>
           <div className={styles.exerciseSelectBlock}>
@@ -316,11 +607,11 @@ const createCourse = () => {
           </div>
           {exercises.map(el => (
             <EditLessonHomeworkComponent el={el} selectedExercise={selectedExercise} firstExerPress={firstExerPress}
-            handleSubmitIsClicked={handleSubmitIsClicked}
-            setHandleSubmitIsClicked={setHandleSubmitIsClicked}
-            lessonTitle={lessonTitle}
-            lessonDesc={lessonDesc}
-            lesson={lesson}
+              handleSubmitIsClicked={handleSubmitIsClicked}
+              setHandleSubmitIsClicked={setHandleSubmitIsClicked}
+              lessonTitle={lessonTitle}
+              lessonDesc={lessonDesc}
+              lesson={lesson}
             />
           ))}
 
